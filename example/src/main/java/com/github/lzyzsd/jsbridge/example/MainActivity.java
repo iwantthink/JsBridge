614b81b21192a699aa3d41e76766075aecd3690b
renbo <renbo.ma@hypers.com>
2017-12-22 18:50:21 +0800

    update

--- example/src/main/java/com/github/lzyzsd/jsbridge/example/MainActivity.java
+++ example/src/main/java/com/github/lzyzsd/jsbridge/example/MainActivity.java
@@ -8,26 +8,22 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.ValueCallback;
-import android.webkit.WebChromeClient;
 import android.widget.Button;
 
-import com.github.lzyzsd.jsbridge.BridgeHandler;
 import com.github.lzyzsd.jsbridge.BridgeWebView;
 import com.github.lzyzsd.jsbridge.CallBackFunction;
-import com.github.lzyzsd.jsbridge.DefaultHandler;
-import com.google.gson.Gson;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	private final String TAG = "MainActivity";
+    private final String TAG = "MainActivity";
 
-	BridgeWebView webView;
+    BridgeWebView webView;
 
-	Button button;
+    Button button;
 
-	int RESULT_CODE = 0;
+    int RESULT_CODE = 0;
 
-	ValueCallback<Uri> mUploadMessage;
+    ValueCallback<Uri> mUploadMessage;
 
     static class Location {
         String address;
@@ -39,98 +35,100 @@ public class MainActivity extends Activity implements OnClickListener {
         String testStr;
     }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        webView = (BridgeWebView) findViewById(R.id.webView);
+        startActivity(new Intent(MainActivity.this, SeconedActivity.class));
+        finish();
 
-		button = (Button) findViewById(R.id.button);
+//        webView = (BridgeWebView) findViewById(R.id.webView);
+//
+//        button = (Button) findViewById(R.id.button);
+//
+//        button.setOnClickListener(this);
+//
+//        webView.setDefaultHandler(new DefaultHandler());
+//
+//        webView.setWebChromeClient(new WebChromeClient() {
+//
+//            @SuppressWarnings("unused")
+//            public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType, String capture) {
+//                this.openFileChooser(uploadMsg);
+//            }
+//
+//            @SuppressWarnings("unused")
+//            public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType) {
+//                this.openFileChooser(uploadMsg);
+//            }
+//
+//            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+//                mUploadMessage = uploadMsg;
+//                pickFile();
+//            }
+//        });
+//
+//        webView.loadUrl("file:///android_asset/demo.html");
+//
+//        webView.registerHandler("submitFromWeb", new BridgeHandler() {
+//
+//            @Override
+//            public void handler(String data, CallBackFunction function) {
+//                Log.i(TAG, "handler = submitFromWeb, data from web = " + data);
+//                function.onCallBack("submitFromWeb exe, response data 中文 from Java");
+//            }
+//
+//        });
+//
+//        User user = new User();
+//        Location location = new Location();
+//        location.address = "SDU";
+//        user.location = location;
+//        user.name = "大头鬼";
+//
+//        webView.callHandler("functionInJs", new Gson().toJson(user), new CallBackFunction() {
+//            @Override
+//            public void onCallBack(String data) {
+//
+//            }
+//        });
+//
+//        webView.send("hello");
 
-		button.setOnClickListener(this);
+    }
 
-		webView.setDefaultHandler(new DefaultHandler());
+    public void pickFile() {
+        Intent chooserIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        chooserIntent.setType("image/*");
+        startActivityForResult(chooserIntent, RESULT_CODE);
+    }
 
-		webView.setWebChromeClient(new WebChromeClient() {
-
-			@SuppressWarnings("unused")
-			public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType, String capture) {
-				this.openFileChooser(uploadMsg);
-			}
-
-			@SuppressWarnings("unused")
-			public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType) {
-				this.openFileChooser(uploadMsg);
-			}
-
-			public void openFileChooser(ValueCallback<Uri> uploadMsg) {
-				mUploadMessage = uploadMsg;
-				pickFile();
-			}
-		});
-
-		webView.loadUrl("file:///android_asset/demo.html");
-
-		webView.registerHandler("submitFromWeb", new BridgeHandler() {
-
-			@Override
-			public void handler(String data, CallBackFunction function) {
-				Log.i(TAG, "handler = submitFromWeb, data from web = " + data);
-                function.onCallBack("submitFromWeb exe, response data 中文 from Java");
-			}
-
-		});
-
-        User user = new User();
-        Location location = new Location();
-        location.address = "SDU";
-        user.location = location;
-        user.name = "大头鬼";
-
-        webView.callHandler("functionInJs", new Gson().toJson(user), new CallBackFunction() {
-            @Override
-            public void onCallBack(String data) {
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == RESULT_CODE) {
+            if (null == mUploadMessage) {
+                return;
             }
-        });
+            Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+    }
 
-        webView.send("hello");
-
-	}
-
-	public void pickFile() {
-		Intent chooserIntent = new Intent(Intent.ACTION_GET_CONTENT);
-		chooserIntent.setType("image/*");
-		startActivityForResult(chooserIntent, RESULT_CODE);
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		if (requestCode == RESULT_CODE) {
-			if (null == mUploadMessage){
-				return;
-			}
-			Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
-			mUploadMessage.onReceiveValue(result);
-			mUploadMessage = null;
-		}
-	}
-
-	@Override
-	public void onClick(View v) {
-		if (button.equals(v)) {
+    @Override
+    public void onClick(View v) {
+        if (button.equals(v)) {
             webView.callHandler("functionInJs", "data from Java", new CallBackFunction() {
 
-				@Override
-				public void onCallBack(String data) {
-					// TODO Auto-generated method stub
-					Log.i(TAG, "reponse data from js " + data);
-				}
+                @Override
+                public void onCallBack(String data) {
+                    Log.i(TAG, "reponse data from js " + data);
+                }
 
-			});
-		}
+            });
+        }
 
-	}
+    }
 
 }
---
+++ example/src/main/java/com/github/lzyzsd/jsbridge/example/SeconedActivity.java
@@ -0,0 +1,139 @@
+package com.github.lzyzsd.jsbridge.example;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsPromptResult;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class SeconedActivity extends AppCompatActivity implements View.OnClickListener {
+
+    WebView mWebView;
+    Button mBtnTest1, mBtnTest2, mBtnTest3, mBtnTest4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seconed);
+        mWebView = (WebView) findViewById(R.id.wv_test);
+        mBtnTest1 = (Button) findViewById(R.id.btn_test1);
+        mBtnTest2 = (Button) findViewById(R.id.btn_test2);
+        mBtnTest3 = (Button) findViewById(R.id.btn_test3);
+        mBtnTest4 = (Button) findViewById(R.id.btn_test4);
+
+        mBtnTest1.setOnClickListener(this);
+        mBtnTest2.setOnClickListener(this);
+        mBtnTest3.setOnClickListener(this);
+        mBtnTest4.setOnClickListener(this);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String str) {
+                Log.d("SeconedActivity", "shouldOverrideUrlLoading");
+                // 步骤2：根据协议的参数，判断是否是所需要的url
+                // 一般根据scheme（协议格式） & authority（协议名）判断（前两个参数）
+                //假定传入进来的 url = "js://webview?arg1=111&arg2=222"（同时也是约定好的需要拦截的）
+                Uri uri = Uri.parse(str);
+                // 如果url的协议 = 预先约定的 js 协议
+                // 就解析往下解析参数
+                if (uri.getScheme().equals("js")) {
+
+                    // 如果 authority  = 预先约定协议里的 webview，即代表都符合约定的协议
+                    // 所以拦截url,下面JS开始调用Android需要的方法
+                    if (uri.getAuthority().equals("webview")) {
+
+                        //  步骤3：
+                        // 执行JS所需要调用的逻辑
+                        Toast.makeText(SeconedActivity.this, "js调用了Android的方法", Toast.LENGTH_SHORT).show();
+                        // 可以在协议上带有参数并传递到Android上
+                        HashMap<String, String> params = new HashMap<>();
+                        Set<String> collection = uri.getQueryParameterNames();
+
+                    }
+
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, str);
+            }
+        });
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+                Log.d("SeconedActivity", "onJsPrompt");
+                return super.onJsPrompt(view, url, message, defaultValue, result);
+            }
+        });
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+
+        mWebView.addJavascriptInterface(new Object() {
+            @JavascriptInterface
+            public void hello() {
+                Toast.makeText(SeconedActivity.this, "i am called by js", Toast.LENGTH_SHORT).show();
+            }
+
+            @JavascriptInterface
+            public void hello2(String msg) {
+                Toast.makeText(SeconedActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+
+            @JavascriptInterface
+            public void hello3() {
+                mWebView.loadUrl("javascript:functionName()");
+            }
+
+        }, "test_name");
+        
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mWebView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mWebView.onPause();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_test1:
+                mWebView.loadUrl("file:///android_asset/sample.html");
+                break;
+            case R.id.btn_test2:
+                //无参数 无返回
+                mWebView.loadUrl("javascript:java2Js1()");
+                break;
+            case R.id.btn_test3:
+                //有参数 无返回
+                mWebView.loadUrl("javascript:java2Js2(\"from native\")");
+                break;
+            case R.id.btn_test4:
+                mWebView.evaluateJavascript("javascript:java2Js3()", new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String value) {
+                        Toast.makeText(SeconedActivity.this, value, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                break;
+        }
+    }
+}
